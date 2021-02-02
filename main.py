c3b59c08f9d7b86aed52a7fa4740e62ec78df60a
rg <rgenius@yandex.ru>
2021-02-02 19:37:45 +0300

    Add function split colors to b, g, r

--- main.py
+++ main.py
@@ -30,30 +30,43 @@ cv2.imshow('Image 1', img1)
 # В openCV обычно используется BGR
 
 # BGR to GrayScale
-gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Gray', gray)
-
-# BGR to HSV
-hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HLS)
-cv2.imshow('HSV', hsv)
-
-
-cv2.waitKey(0)
+# gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('Gray', gray)
+#
+# # BGR to HSV
+# hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HLS)
+# cv2.imshow('HSV', hsv)
+#
+# # BGR to LAB
+# lab = cv2.cvtColor(img1, cv2.COLOR_BGR2LAB)
+# cv2.imshow('LAB', lab)
+#
+# b,g,r = cv2.split(img1)
+#
+# cv2.imshow('Blue', b)
+# cv2.imshow('Green', g)
+# cv2.imshow('Red', r)
+#
+# cv2.waitKey(0)
 
 
 #Reading videos
-# capture = cv2.VideoCapture('Media/Videos/1.mp4')
-#
-#
-# while True:
-#     isTrue, frame = capture.read()
-#     if isTrue:
-#         cv2.imshow('Video', frame)
-#
-#     if cv2.waitKey(20) & 0xFF==ord('d'):
-#         break
-#
-# capture.release()
-# cv2.destroyAllWindows()
-#
-# cv2.waitKey(0)
+capture = cv2.VideoCapture('Media/Videos/1.mp4')
+
+
+while True:
+    isTrue, frame = capture.read()
+
+
+    if isTrue:
+        b, g, r = cv2.split(frame)
+        cv2.imshow('Video', frame)
+        cv2.imshow('Green', g)
+
+    if cv2.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+
+cv2.waitKey(0)
