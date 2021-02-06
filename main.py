e847472894b47f00058999b6801e734a16ea7e49
rg <rgenius@yandex.ru>
2021-02-06 16:11:26 +0300

    Some changes for the first task

--- main.py
+++ main.py
@@ -7,27 +7,32 @@ img2 = cv2.imread('Media/Photos/Snow-man2.jpeg')
 img3 = cv2.imread('Media/Photos/arms_6x6.png', cv2.IMREAD_GRAYSCALE)
 img4 = cv2.imread('Media/Photos/arms_64x64.png')
 
-img4_scale = cv2.resize(img4, (200, 200))
 
-#print(img1.shape)
+print(img1.shape)  # Вывод в косоль размер изображения и кол-во цветов
 
 
-print(img3 + 50)
-# print(type(img3))
+# Обрезка изображения (crop)
+cv2.imshow('Image 1 croped', img1[:60, 500:560])
 
-#cv2.imshow('Image 1', img1[:6, 500:506])
 
-cv2.imshow('Image 3', img3)
-cv2.imshow('Image 3 + Brightness', img3 + 50)
+# Изменение яркости напрямую (не рекомендуется)
+cv2.imshow('Image 3', img3)  # Исходное изображение
+cv2.imshow('Image 3 + Brightness', img3 + 50)  # Изменнное изображение
+print(img3 + 50)  # Вывод в консоль таблицы пикселей
 
-cv2.imshow('Image 4', img4)
-cv2.imshow('Image 4 200x200', img4_scale)
+
+# Изменение размеров изображения изображения
+img4_scale = cv2.resize(img4, (200, 200))  # Функция изменения размера
+
+cv2.imshow('Image 4', img4)  # Исходное изображение
+cv2.imshow('Image 4 200x200', img4_scale)  # Изменнное изображение
+
 
 # BGR исходное изображение
-cv2.imshow('Image 1', img1)
+# cv2.imshow('Image 1', img1)
 
-# Обычно используется в изображениях RGB
-# В openCV обычно используется BGR
+# Обычно в изображениях используется цветовое пространство RGB
+# В openCV обычно используется - BGR
 
 # BGR to GrayScale
 # gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -50,23 +55,22 @@ cv2.imshow('Image 1', img1)
 # cv2.waitKey(0)
 
 
-#Reading videos
-capture = cv2.VideoCapture('Media/Videos/1.mp4')
+# Reading videos
+# capture = cv2.VideoCapture('Media/Videos/1.mp4')
+#
+# while True:
+#     isTrue, frame = capture.read()
+#
+#     if isTrue:
+#         b, g, r = cv2.split(frame)
+#         cv2.imshow('Video', frame)
+#         cv2.imshow('Green', g)
+#
+#     if cv2.waitKey(20) & 0xFF==ord('d'):
+#         break
+#
+# capture.release()
+# cv2.destroyAllWindows()
 
-
-while True:
-    isTrue, frame = capture.read()
-
-
-    if isTrue:
-        b, g, r = cv2.split(frame)
-        cv2.imshow('Video', frame)
-        cv2.imshow('Green', g)
-
-    if cv2.waitKey(20) & 0xFF==ord('d'):
-        break
-
-capture.release()
-cv2.destroyAllWindows()
 
 cv2.waitKey(0)
