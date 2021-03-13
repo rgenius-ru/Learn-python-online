754e29c322c474c7e7cab90a6c402d84ea2ced77
rg <rgenius@yandex.ru>
2021-03-13 15:25:55 +0300

    Add section: face detect on video

---
+++ faces_detect_on_video.py
@@ -0,0 +1,54 @@
+# Ссылка на некоторые таблицы haar cascade:
+# https://github.com/opencv/opencv/tree/master/data/haarcascades
+
+# Импортируем библиотеку opencv
+import cv2
+
+
+def modify_frame(_img, scale=1.0):
+    img_copy = _img.copy()  # Копируем исходное изображение в новую переменую
+
+    # Изменяем размер исходного изображения
+    # scale = 1.0  # Масштаб в долях процента
+    img_copy = cv2.resize(img_copy, None, fx=scale, fy=scale)  # Изменение размера
+    # cv2.imshow('Source image', img)  # Вывод исходного изображения
+
+    gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)  # Преобразовываем в серый цвет
+    # cv2.imshow('Gray', gray)  # Вывод серого изображения
+
+    haar_cascade = cv2.CascadeClassifier('haar_face.xml')  # Загружаем касадную таблицу
+    faces_rect = haar_cascade.detectMultiScale(  # Функция библиотеки opencv для детекции объекта
+        gray,  # Изображение
+        scaleFactor=1.1,  # Коэффициент увеличения (min = 1.1)
+        minNeighbors=10  # Коэффициент нахождения объекта минимального размера
+    )  # Возвращает прямоугольники найденных объектов (номеров)
+
+    print('Number of faces found = ', len(faces_rect))  # Выводим количество найденных лиц
+
+    for (x, y, w, h) in faces_rect:  # Для каждого прямоугольника
+        cv2.rectangle(img_copy, (x, y), (x+w, y+h), (150, 255, 60), thickness=4)  # Рисуем прямоугольники
+
+    return img_copy
+
+
+# Читаем видеопоток в бесконечном цикле
+while True:
+    is_closed = 0
+    cap = cv2.VideoCapture('Media/Videos/film_1.mp4')
+
+    while True:
+        ret, frame = cap.read()  # Считываем кадр
+        if ret:
+            frame = modify_frame(frame, 0.5)  # Ищем контуры и накладываем их на кадр
+            cv2.imshow('Video', frame)  # Выводим кадр на экран
+            if cv2.waitKey(1) == 27:
+                is_closed = 1  # По нажатию [Esc] в isclosed запишется 1
+                break
+        else:
+            break
+
+    if is_closed:
+        break
+
+cap.release()  # Освобождаем память от загруженного видео
+cv2.destroyAllWindows()  # Уничтожаем все окна
